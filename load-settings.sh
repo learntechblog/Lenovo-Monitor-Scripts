b36fb31b526e9fd9f462733acbb44aee8b9882e7
Learn <learntechblog@rrproject.com>
2010-02-02 09:12:30 -0600

    removed sleep statements, i think the second load statement should take care of things

--- load-settings.sh
+++ load-settings.sh
@@ -14,13 +14,12 @@ fi
 
 # Import disper settings if they exist
 disper -i < "$SCRIPTPATH/$1/disper-settings"
-sleep 2;
 
 # Reset, then import panels xml if it's there
 gconftool --load "$SCRIPTPATH/blank-panels.xml"
-# Wait 2 seconds
-sleep 2;
 gconftool --load "$SCRIPTPATH/$1/panels.xml"
+
+# Sometimes the first load attempt fails, so try again.
 gconftool --load "$SCRIPTPATH/$1/panels.xml"
 # I've had issues with gconftool not loading my panels correctly, 
 # the second call seems to fix this.
